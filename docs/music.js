cd5154d86ff26afd1a9d8ce59722a27322f60ccf
cathy-mengying-fang <fangcat16@icloud.com>
2020-07-10 16:41:15 -0400

    tone.js

---
+++ docs/music.js
@@ -0,0 +1,16 @@
+import * as Tone from "tone";
+
+
+//attach a click listener to a play button
+document.querySelector('button').addEventListener('click', async () => {
+    await Tone.start()
+    console.log('audio is ready')
+    //create a synth and connect it to the master output (your speakers)
+    const synth = new Tone.Synth().toMaster();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
+
+
+
+})
