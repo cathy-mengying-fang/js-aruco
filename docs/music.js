3ce1cff33c655a25183b622238ae6a0418c3bf12
cathy-mengying-fang <fangcat16@icloud.com>
2020-07-13 13:03:43 -0400

    play note success

--- docs/music.js
+++ docs/music.js
@@ -1,10 +1,8 @@
-import * as Tone from "tone/build/Tone.js";
-
 
 //attach a click listener to a play button
 document.querySelector('button').addEventListener('click', async () => {
     await Tone.start()
-    console.log('audio is ready')
+    
     //create a synth and connect it to the master output (your speakers)
     const synth = new Tone.Synth().toMaster();
 
